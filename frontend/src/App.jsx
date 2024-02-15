4b8571a5967e946cfe60af9b9aa60f47a5eb41cc
AFilippov <64398851+WebAFilippov@users.noreply.github.com>
2024-02-16 04:50:04 +0700

    add primitive layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,13 @@
+import { Route, Routes } from "react-router-dom"
 import { Navbar } from "./components/navbar.component"
 
 export const App = () => {
-  return <Navbar />
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="signin" element={<p>das</p>} />
+        <Route path="signup" element={<p>qwerty</p>} />
+      </Route>
+    </Routes>
+  )
 }
