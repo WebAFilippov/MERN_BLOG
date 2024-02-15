39dd4411574df3556bf5f8cdd51203ce1364ceda
AFilippov <64398851+WebAFilippov@users.noreply.github.com>
2024-02-15 23:14:12 +0700

    add alias

--- server/Schema/User.js
+++ server/Schema/User.js
@@ -4,90 +4,88 @@ let profile_imgs_name_list = ["Garfield", "Tinkerbell", "Annie", "Loki", "Cleo",
 let profile_imgs_collections_list = ["notionists-neutral", "adventurer-neutral", "fun-emoji"];
 
 const userSchema = mongoose.Schema({
-
-    personal_info: {
-        fullname: {
-            type: String,
-            lowercase: true,
-            required: true,
-            minlength: [3, 'fullname must be 3 letters long'],
-        },
-        email: {
-            type: String,
-            required: true,
-            lowercase: true,
-            unique: true
-        },
-        password: String,
-        username: {
-            type: String,
-            minlength: [3, 'Username must be 3 letters long'],
-            unique: true,
-        },
-        bio: {
-            type: String,
-            maxlength: [200, 'Bio should not be more than 200'],
-            default: "",
-        },
-        profile_img: {
-            type: String,
-            default: () => {
-                return `https://api.dicebear.com/6.x/${profile_imgs_collections_list[Math.floor(Math.random() * profile_imgs_collections_list.length)]}/svg?seed=${profile_imgs_name_list[Math.floor(Math.random() * profile_imgs_name_list.length)]}`
-            } 
-        },
+  personal_info: {
+    fullname: {
+      type: String,
+      lowercase: true,
+      required: true,
+      minlength: [3, 'Имя должно состоять минимум из 3 букв'],
     },
-    social_links: {
-        youtube: {
-            type: String,
-            default: "",
-        },
-        instagram: {
-            type: String,
-            default: "",
-        },
-        facebook: {
-            type: String,
-            default: "",
-        },
-        twitter: {
-            type: String,
-            default: "",
-        },
-        github: {
-            type: String,
-            default: "",
-        },
-        website: {
-            type: String,
-            default: "",
-        }
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true
     },
-    account_info:{
-        total_posts: {
-            type: Number,
-            default: 0
-        },
-        total_reads: {
-            type: Number,
-            default: 0
-        },
+    password: String,
+    username: {
+      type: String,
+      minlength: [3, 'Никнейм должно состоять минимум из 3 букв'],
+      unique: true,
     },
-    google_auth: {
-        type: Boolean,
-        default: false
+    bio: {
+      type: String,
+      maxlength: [200, 'О себе не должно быть больше 200 символов'],
+      default: "",
     },
-    blogs: {
-        type: [ Schema.Types.ObjectId ],
-        ref: 'blogs',
-        default: [],
+    profile_img: {
+      type: String,
+      default: () => {
+        return `https://api.dicebear.com/6.x/${profile_imgs_collections_list[Math.floor(Math.random() * profile_imgs_collections_list.length)]}/svg?seed=${profile_imgs_name_list[Math.floor(Math.random() * profile_imgs_name_list.length)]}`
+      }
+    },
+  },
+  social_links: {
+    youtube: {
+      type: String,
+      default: "",
+    },
+    instagram: {
+      type: String,
+      default: "",
+    },
+    facebook: {
+      type: String,
+      default: "",
+    },
+    twitter: {
+      type: String,
+      default: "",
+    },
+    github: {
+      type: String,
+      default: "",
+    },
+    website: {
+      type: String,
+      default: "",
     }
-
-}, 
-{ 
+  },
+  account_info: {
+    total_posts: {
+      type: Number,
+      default: 0
+    },
+    total_reads: {
+      type: Number,
+      default: 0
+    },
+  },
+  google_auth: {
+    type: Boolean,
+    default: false
+  },
+  blogs: {
+    type: [Schema.Types.ObjectId],
+    ref: 'blogs',
+    default: [],
+  }
+},
+  {
     timestamps: {
-        createdAt: 'joinedAt'
-    } 
-
-})
+      createdAt: 'joinedAt'
+    }
+  }
+)
 
 export default mongoose.model("users", userSchema);
